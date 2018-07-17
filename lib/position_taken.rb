b34f344741b1dbc207225ce0565a5aaaec42df42
Ignacio R Espino <irespino@capozios.com>
2018-07-17 21:04:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,6 @@
 
 def position_taken?(board, index)
   tempPos = board[index]
-  if index == nil
-    return false
-  end
   if tempPos == " " || tempPos == "" || tempPos == nil
     return false
   else
