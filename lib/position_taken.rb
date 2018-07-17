495e1e033164a16b648d5b31f937cdbd00c4f076
Ignacio R Espino <irespino@capozios.com>
2018-07-17 20:58:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken(index)
+  tempPos = board[index]
+  if tempPos == "X" || tempPos == "O"
+    return true
+  else
+    return false
+  end
+end
